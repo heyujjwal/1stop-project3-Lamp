0b8dd7107e81e95313a3a71ee74719154d1e6b0a
Ujjwal Gupta <guptaujjwal259@gmail.com>
2024-09-03 11:33:26 +0530

    added files

---
+++ resources/views/index1.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>My Cognito Application</title>
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    </head>
+    <body>
+        <div class="container p-5">
+            <center>
+            <div class="card" style="width:600px;height:250px !important">
+                <p class='p-5'>Welcome {{$data->email}}!,your session has been created!</p>
+            </div>
+            </center>
+           
+        </div>
+        
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,32 @@
 <?php
-
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/', function (Request $request) {
+
+    $grantCode=$request->get('code');
+    $response = Http::asForm()->withHeaders([
+        'Content-Type'=>'application/x-www-form-urlencoded',
+        'Authorization'=>'Basic NHJhbmpnNTBwMG5pdHA0ZWFkMWxjYzUzZmc6MTFhbWdiNXNrZml2N3J0MHZndHA0ZzAyNW5kcWo4MTdrbG1jYmIxZXFubWlwbmRqa2w0bA==' 
+    ])->post('https://ujjwalsnippet.auth.us-west-2.amazoncognito.com/oauth2/token',[
+        'grant_type'=>'authorization_code' ,
+        'code'=>$grantCode ,
+        'redirect_uri'=>'http://localhost:8000/'
+    ]);
+
+
+    $decodedResponse= json_decode($response);
+    $accessToken = $decodedResponse->access_token;
+    
+    $userInfoResponse = Http::asForm()->withHeaders([
+        'Content-Type'=>'application/x-www-form-urlencoded',
+        'Authorization'=>'Bearer '.$accessToken 
+    ])->get('https://ujjwalsnippet.auth.us-west-2.amazoncognito.com/oauth2/userInfo');
+
+    $data = json_decode($userInfoResponse);
+
+    return view('index1',['data'=>$data]);
 });
+
